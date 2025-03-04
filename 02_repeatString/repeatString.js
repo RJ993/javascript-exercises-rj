0faa55a584153196e8040c6618d4fe20a944f996
RJ993 <196080894+RJ993@users.noreply.github.com>
2025-03-04 13:56:16 -0500

    Finish Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, number) {
+    if(number < 0) return "ERROR";
+    let phrase = ''
+    for(i = 0; i < number; i++){
+        phrase += word
+    }
+    return phrase;
 };
 
 // Do not edit below this line
