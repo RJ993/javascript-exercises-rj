c0d34ea82c2cc66a7f260b66507f0c582e88b7e9
RJ993 <196080894+RJ993@users.noreply.github.com>
2025-03-05 11:49:10 -0500

    Finish Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(initialNumber, finishingNumber) {
+    let finalSum = 0
+    if(initialNumber < 0 || finishingNumber < 0){
+        return "ERROR"
+    }else if(!Number.isInteger(initialNumber) || !Number.isInteger(finishingNumber)){
+        return "ERROR"
+    }else if (initialNumber < finishingNumber){
+        for(i = initialNumber; i <= finishingNumber; i++){
+            finalSum += initialNumber++
+        }
+        return parseInt(finalSum)
+    }else if (initialNumber > finishingNumber){
+        for(i = finishingNumber; i <= initialNumber; i++){
+            finalSum += finishingNumber++
+        }
+        return parseInt(finalSum)
+    }
+
 
 };
 
