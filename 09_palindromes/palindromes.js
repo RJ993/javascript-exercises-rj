4df3ab6b2c735a3ec813d2f2fc53050c55fec3f2
RJ993 <196080894+RJ993@users.noreply.github.com>
2025-03-21 14:54:18 -0400

    Finish Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (orgPhase) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    let newPhase = orgPhase.toLowerCase().split("").filter((character) => alphanumerical.includes(character)).join("")
+    let revPhase = newPhase.split("").reverse().join("")
+    return revPhase === newPhase
 };
 
 // Do not edit below this line
